89339744333672935cde9734975acc728535908f
Stephen <Stephen@O-Connor-ToshL>
2015-01-18 17:09:22 +0000

    Ascending Descending bars

--- 06-BarChart-Column/script.js
+++ 06-BarChart-Column/script.js
@@ -95,96 +95,195 @@
    var chart = svg.append( 'g' )
            .classed( 'display', true )
            .attr( 'transform', 'translate(' + margin.left + ',' + margin.top + ')' );
+  var controls = d3.select('body')
+                  .append('div')  
+                  .attr('id', 'controls');
+  var sort_btn = controls.append('button')
+                  .html('Sort data: assending')
+                  .attr('state', 0);
+   drawAxis = function(params)
+   {
+      if(params.initialize === true){
+          // draw the gridlines and axis
+          // draw the gridlines
+          this.append('g')
+              .call(yGridlines)
+              .classed('gridline', true)
+              .attr('transform', 'translate(0,0)')
 
+          // x axis
+          this.append('g')
+                  .classed('x axis', true)
+                  .attr('transform', "translate(" + 0 + "," + height + ")")
+                  .call(params.axis.x)
+                      .selectAll('text')
+                      .classed('x-axis-label', true)
+                          .style('text-anchor', 'end')
+                          .attr('dx', -8)
+                          .attr('dy', 8)
+                          .attr('transform', 'translate(0,0) rotate(-45)'); 
+          // y axis
+          this.append('g')
+                  .classed('y axis', true)
+                  .attr('transform', "translate(0,0)")
+                  .call(params.axis.y);
+
+          // y label
+          this.select('.y.axis')
+                  .append('text')
+                  .attr('x', 0)
+                  .attr('y', 0)
+                  .style('text-anchor', 'middle')
+                  .attr('transform', 'translate(-50, ' + height/2 + ') rotate(-90)')
+                  .text('Units Sold'); 
+
+          // x label
+          this.select('.x.axis')
+                  .append('text')
+                  .attr('x', 0)
+                  .attr('y', 0)
+                  .style('text-anchor', 'middle')
+                  .attr('transform', 'translate(' + width/2 + ', 80)')
+                  .text('Donut Type'); 
+      } else if(params.initialize === false)
+      {
+          // update info
+          this.selectAll('g.x.axis')
+            .call(params.axis.x) 
+          this.selectAll('.x-axis-label')           
+              .style('text-anchor', 'end')
+                  .attr('dx', -8)
+                  .attr('dy', 8)
+                  .attr('transform', 'translate(0,0) rotate(-45)')
+          this.selectAll('g.y.axis')
+            .call(params.axis.y)
+      }
+   }
    plot = function ( params )
    {
-      this.append('g')
-          .call(yGridlines)
-          .classed('gridline', true)
-          .attr('transform', 'translate(0,0)')
+    x.domain( data.map( function ( entry )
+           {
+              return entry.key;
+           } ) );
+    y.domain( [ 0, d3.max( data, function ( d )
+              {
+                 return d.value;
+              } ) ] );
+
+      drawAxis.call(this, params);
+      
+      // enter()
       this.selectAll( ".bar" )
               .data( params.data )
               .enter()
-              .append( 'rect' )
-              .classed( 'bar', 'true' )
-              .attr( 'x', function(d, i)
-              {
-                return x(d.key);
-              } )
-              .attr( 'y', function ( d, i )
-              {
-                 return y( d.value );
-              } )
-              .attr( 'height', function ( d, i )
-              {
-                 return height - y(d.value);
-              } )
-              .attr( 'width', function ( d )
-              {
-                 return x.rangeBand();
-              } )
-              .style( "fill", function ( d, i )
-              {
-                return ordinalColorScale(i);
-                 // return linearColorScale( i );
-              } );
+                .append( 'rect' )
+                .classed( 'bar', 'true' );
+
       this.selectAll( ".bar-label" )
               .data( params.data )
               .enter()
               .append( 'text' )
-              .classed( 'bar-label', true )
-              .attr( 'x', function ( d , i )
-              {
-                 return x( d.key ) + ( x.rangeBand()/2);
-              } )
-              .attr( 'dx', 0 )
-              .attr( 'y', function ( d, i )
-              {
-                 return y( d.value );
-              } )
-              .attr( 'dy', -6 )
-              .text( function ( d )
-              {
-                 return d.value;
-              } )
-      this.append('g')
-              .classed('x axis', true)
-              .attr('transform', "translate(" + 0 + "," + height + ")")
-              .call(params.axis.x)
-                  .selectAll('text')
-                      .style('text-anchor', 'end')
-                      .attr('dx', -8)
-                      .attr('dy', 8)
-                      .attr('transform', 'translate(0,0) rotate(-45)'); 
+              .classed( 'bar-label', true );
 
-      this.append('g')
-              .classed('y axis', true)
-              .attr('transform', "translate(0,0)")
-              .call(params.axis.y);
+      // update()
+      this.selectAll('.bar')
+          .attr( 'x', function(d, i)
+          {
+            return x(d.key);
+          } )
+          .attr( 'y', function ( d, i )
+          {
+             return y( d.value );
+          } )
+          .attr( 'height', function ( d, i )
+          {
+             return height - y(d.value);
+          } )
+          .attr( 'width', function ( d )
+          {
+             return x.rangeBand();
+          } )
+          .style( "fill", function ( d, i )
+          {
+            return ordinalColorScale(i);             
+          } );
 
-      this.select('.y.axis')
-              .append('text')
-              .attr('x', 0)
-              .attr('y', 0)
-              .style('text-anchor', 'middle')
-              .attr('transform', 'translate(-50, ' + height/2 + ') rotate(-90)')
-              .text('Units Sold'); 
+      this.selectAll('.bar-label')
+          .attr( 'x', function ( d , i )
+          {
+             return x( d.key ) + ( x.rangeBand()/2);
+          } )
+          .attr( 'dx', 0 )
+          .attr( 'y', function ( d, i )
+          {
+             return y( d.value );
+          } )
+          .attr( 'dy', -6 )
+          .text( function ( d )
+          {
+             return d.value;
+          } )
 
-      this.select('.x.axis')
-              .append('text')
-              .attr('x', 0)
-              .attr('y', 0)
-              .style('text-anchor', 'middle')
-              .attr('transform', 'translate(' + width/2 + ', 80)')
-              .text('Donut Type'); 
+      // exit()
+      this.selectAll('.bar')
+          .data(params.data)
+          .exit()
+          .remove();
+
+      this.selectAll('.bar-label')
+          .data(params.data)
+          .exit()
+          .remove();
+
+      
    };
-   plot.call( chart, {
+
+   sort_btn.on('click', function()
+   {
+    var self = d3.select(this);
+    var ascending = function(a, b)
+    {
+      return a.value - b.value;
+    };
+    var descending = function(a, b)
+    {
+      return b.value - a.value;
+    };
+    var state = +self.attr('state');
+    var txt = 'Sort data: ';
+      if(state === 0)
+      {
+        data.sort(ascending);
+        state = 1;
+        txt += 'descending';
+      } else if(state === 1)
+      {
+        data.sort(descending);
+          state = 0;
+          txt += 'ascending';
+      }
+      self.attr('state', state);
+      self.html(txt);
+
+      plot.call( chart, {
       data: data,
       axis: 
               {
                  x: xAxis,
                  y: yAxis
               },
-              gridlines: yGridlines 
+              gridlines: yGridlines,
+              initialize: false
+   } );
+   });
+plot.call( chart, {
+  data: data,
+  axis: 
+          {
+             x: xAxis,
+             y: yAxis
+          },
+          gridlines: yGridlines,
+          initialize: true
    } );
 }() );
