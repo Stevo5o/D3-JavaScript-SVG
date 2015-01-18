70cb1a45d697443978b75b52557e267ce25a7d34
Stephen <Stephen@O-Connor-ToshL>
2015-01-18 13:09:52 +0000

    margin

--- 05-BarChart/script.js
+++ 05-BarChart/script.js
@@ -3,39 +3,53 @@
 ( function () {
    var w = 800;
    var h = 450;
+   var margin = {
+      top: 20,
+      bottom: 20,
+      left: 20,
+      right: 20 
+   };
+   var width = w - margin.left - margin.right;
+   var height = h - margin.top - margin.bottom;
+
    var data = [ 132, 71, 337, 93, 78, 43, 20, 16, 30, 8, 17, 21 ];
    var x = d3.scale.linear()
            .domain( [ 0, d3.max( data ) ] )
-           .range( [ 0, w ] );
+           .range( [ 0, width ] );
    var y = d3.scale.linear()
            .domain( [ 0, data.length ] )
-           .range( [ 0, h ] );
+           .range( [ 0, height ] );
 
    var svg = d3.select( "body" ).append( 'svg' )
            .attr( 'id', 'chart' )
            .attr( 'width', w )
            .attr( 'height', h );
+   var chart = svg.append( 'g' )
+           .classed( 'display', true )
+           .attr( 'transform', 'translate(' + margin.left + ',' + margin.top + ')' );
 
-   svg.selectAll( ".bar" )
-           .data( data )
+plot = function(params)
+{  
+  this.selectAll( ".bar" )
+           .data( params.data )
            .enter()
-           .append( 'rect' )
-           .classed( 'bar', 'true' )
-           .attr( 'x', 0 )
-           .attr( 'y', function ( d, i )
-           {
-              return y( i );
-           } )
-           .attr( 'width', function ( d, i )
-           {
-              return x( d );
-           } )
-           .attr( 'height', function ( d, i )
-           {
-              return y( 1 ) - 1;
-           } );
-   svg.selectAll( ".bar-label" )
-           .data( data )
+             .append( 'rect' )
+             .classed( 'bar', 'true' )
+             .attr( 'x', 0 )
+             .attr( 'y', function ( d, i )
+             {
+                return y( i );
+             } )
+             .attr( 'width', function ( d, i )
+             {
+                return x( d );
+             } )
+             .attr( 'height', function ( d, i )
+             {
+                return y( 1 ) - 1;
+             } );
+   this.selectAll( ".bar-label" )
+           .data( params.data )
            .enter()
             .append( 'text' )
             .classed( 'bar-label', true )
@@ -56,4 +70,9 @@
             {
                return d;
             } );
+}  
+plot.call(chart, {
+  data: data
+});
+   
 }() );
