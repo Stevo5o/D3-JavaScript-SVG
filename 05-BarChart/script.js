b65d79d85fd4776d9b7b754497b939fc2d9a71bc
Stephen <Stephen@O-Connor-ToshL>
2015-01-18 11:36:26 +0000

    Barchart fill svg dx dy and css styling

--- 05-BarChart/script.js
+++ 05-BarChart/script.js
@@ -1,11 +1,16 @@
 /* @ Stephen O'Connor */
 // immediately invoked anonymous function
 ( function () {
-
-   var data = [ 132, 71, 337, 93, 78, 43, 20, 16, 30, 8, 17, 21];
-
    var w = 800;
    var h = 450;
+   var data = [ 132, 71, 337, 93, 78, 43, 20, 16, 30, 8, 17, 21 ];
+   var x = d3.scale.linear()
+           .domain( [ 0, d3.max( data ) ] )
+           .range( [ 0, w ] );
+   var y = d3.scale.linear()
+           .domain( [ 0, data.length ] )
+           .range( [ 0, h ] );
+
    var svg = d3.select( "body" ).append( 'svg' )
            .attr( 'id', 'chart' )
            .attr( 'width', w )
@@ -15,15 +20,40 @@
            .data( data )
            .enter()
            .append( 'rect' )
-           .attr( 'class', 'bar' )
+           .classed( 'bar', 'true' )
            .attr( 'x', 0 )
            .attr( 'y', function ( d, i )
            {
-              return i * 20;
+              return y( i );
            } )
            .attr( 'width', function ( d, i )
            {
-              return d;
+              return x( d );
            } )
-           .attr( 'height', 19 )
+           .attr( 'height', function ( d, i )
+           {
+              return y( 1 ) - 1;
+           } );
+   svg.selectAll( ".bar-label" )
+           .data( data )
+           .enter()
+            .append( 'text' )
+            .classed( 'bar-label', true )
+            .attr( 'x', function ( d, i )
+            {
+               return x( d );
+            } )
+            .attr( 'y', function ( d, i )
+            {
+               return y( i );
+            } )
+            .attr( 'dx', -4 )
+            .attr( 'dy', function ( d, i )
+            {
+               return y( 1 ) / 1.5 + 2;
+            } )
+            .text( function ( d, i )
+            {
+               return d;
+            } );
 }() );
